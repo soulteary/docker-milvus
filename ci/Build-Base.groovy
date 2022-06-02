40f9302ea31d778aabdc73a153c14ea33f3443ca
Jenny Li <jing.li@zilliz.com>
2022-06-02 15:34:18 +0800

    Remove branch
    Signed-off-by: Jenny Li <jing.li@zilliz.com>

--- ci/Build-Base.groovy
+++ ci/Build-Base.groovy
@@ -19,9 +19,6 @@ pipeline {
             customWorkspace '/home/jenkins/agent/workspace'
         }
    }
-    parameters {
-        gitParameter branchFilter: 'origin/(.*)', defaultValue: 'master', name: 'BRANCH', type: 'PT_BRANCH'
-    }
    environment{
       DOCKER_IMAGE="harbor-ap1.zilliz.cc/milvus/${app_name}"
       GITHUB_TOKEN_ID="github-token"
@@ -30,13 +27,11 @@ pipeline {
     stages {
         stage('Checkout'){
             steps{
-                // git branch: "${params.BRANCH}", url: "${github}"
                 script {
                     sh 'printenv'
                     date = sh(returnStdout: true, script: 'date +%Y%m%d').trim()
-
                     gitShortCommit = sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()
-                    image_tag="${params.BRANCH}-${date}-${gitShortCommit}"
+                    image_tag="${date}-${gitShortCommit}"
                 }
             }
         }
@@ -50,9 +45,6 @@ pipeline {
                     --context="`pwd`" \
                     --registry-mirror="nexus-nexus-repository-manager-docker-5000.nexus:5000"\
                     --insecure-registry="nexus-nexus-repository-manager-docker-5000.nexus:5000" \
-                    --build-arg=GIT_REPO=${GIT_REPO} \
-                    --build-arg=GIT_BRANCH=${BRANCH_NAME} \
-                    --build-arg=GIT_COMMIT_HASH=${GIT_COMMIT} \
                     --dockerfile "docker/base/Dockerfile" \
                     --destination=${DOCKER_IMAGE}:${image_tag}
                     """
